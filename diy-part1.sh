c9e71b5e836d7e18ea27cb7752037e35d701ae2f
ababwbq <ababwbq@outlook.com>
2021-06-28 20:11:48 +0800

    添加某插件

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,15 @@
 #
 
 # 取消插件注释
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 
 # 添加插件源码
 # sed -i '$a src-git ddnsto https://github.com/linkease/ddnsto-openwrt' feeds.conf.default
 # sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
 # sed -i '$a src-git dnsfilter https://github.com/garypang13/luci-app-dnsfilter' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
 
 # 添加插件源码
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
