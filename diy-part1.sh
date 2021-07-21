83b12d83c32e9f767188c790a0f2a5465561448f
ababwbq <82033636+ababwbq@users.noreply.github.com>
2021-07-21 10:16:22 +0800

    修改主题测试

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,19 +13,20 @@
 # 取消插件注释
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-
 # 添加插件源码
 # sed -i '$a src-git ddnsto https://github.com/linkease/ddnsto-openwrt' feeds.conf.default
 # sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
 # sed -i '$a src-git dnsfilter https://github.com/garypang13/luci-app-dnsfilter' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-
 # 添加插件源码
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # passwall依赖
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+### 修改主题文件
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 ### 修改为R4A千兆版Breed直刷版
 ## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
